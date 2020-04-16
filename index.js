d4afc4a17b647ce780593cf8d331a4f762e48df6
ramiforde <ramiforde@gmail.com>
2020-04-16 19:58:10 -0400

    Fixed routes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-let express = require("express");
-let fs = require("fs");
+const express = require("express");
+const fs = require("fs");
 const path = require("path");
+const util = require("util");
+const uni = require ("uniqid");
+const fsReadFile = util.promisify(fs.readFile);
+const fsWriteFile = util.promisify(fs.writeFile);
+
 
 
 // Set the port of our application
+
 // process.env.PORT lets the port be set by Heroku
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 3000;
 
 // Create express app instance.
 let app = express();
@@ -16,25 +22,67 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+// Get routes
+app.get("/api/notes",(req,res)=>{
+  fsReadFile(path.join(__dirname,"/db/db.json")).then(response=>{
+      res.json(JSON.parse(response));
+  });
+});
 
-// Create routes for user to take notes
-app.get("/notes", function(req, res) {
+// Post routes
+app.post("/api/notes",(req,res)=>{
+  fsReadFile(path.join(__dirname,"/db/db.json")).then(response=>{
+    
+      let resNotes = JSON.parse(response);
+
+      let uniqueId = uni();
+
+      let notes = {
+          noteId: uniqueId,
+          noteTitle: req.body.noteTitle,
+          noteText: req.body.text
+      }
+
+      resNotes.push(notes);
+      // Then we want to write this to our db.json
+      fsWriteFile(path.join(__dirname,"/db/db.json"),JSON.stringify(resNotes));
+
+      res.end(uniqueId);
+      
+  });
+})
+
+// Dele
+app.delete("/api/notes/:id",(req,res)=>{
+  function deleteNote(note,res){
+
+    fsReadFile(path.join(__dirname,"/db/db.json"))
+    .then(response=>
+      {
+        let resNotes = JSON.parse(response);
+        fsWriteFile(path.join(__dirname,"/db/db.json"),JSON.stringify(resNotes))
+        .then(res=>{
+            res.sendFile(path.join(__dirname, "/public/notes.html"));
+        });
+    });
+  }
+
+  deleteNote(req.params.id,res);
+
+
+});
+
+app.get("/notes",(req,res)=>{
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
-
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
-
-app.get("*", function(req, res) {
+app.get("/*",(req,res)=>{
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, () => {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  }); 
+app.listen(PORT, ()=>{
+console.log(`listening on port ${PORT}`)
+});
+
+
